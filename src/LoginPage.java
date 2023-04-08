b05e02b3fb15106da85976597631308e83ed39bc
Ahmadzainoor <=>
2023-04-08 12:49:37 -0400

    login page code

---
+++ src/LoginPage.java
@@ -0,0 +1,13 @@
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+import javafx.geometry.NodeOrientation;
+
+public class LoginPage {
+
+    public static void main(String[] args) {
+        String userName= "Noor";
+        String Password= "pass123";
+
+        System.out.println("userName is" +UserName + "Password is" + passord);
+
+    }
+}
